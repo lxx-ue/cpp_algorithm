eb46386c8f5d29cf6b90767395548304ed52e7d8
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-08-10 21:38:19 +0300

    binary search

--- cpp_sorts.cpp
+++ cpp_sorts.cpp
@@ -52,6 +52,29 @@ void InsertionSort(vector<int>& values) {
 	*/
 }
 
+int binar_search(vector<int>& v, int item)
+{
+	int low = 0;
+	int high = v.size() - 1;
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		int quess = v[mid];
+		if (quess == item)
+			return mid;
+		if (quess > item)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return -1;
+	/*
+	| Сложность |	Наилучший случай |	В среднем |	Наихудший случай |
+	| Время		|		O(1)		 |	O(log n)	  |		O(log n)		 |
+	| Память	|		O(1)		 |	O(1)	  |		O(1)		 |
+	*/
+}
+
 
 int main()
 {
@@ -80,5 +103,20 @@ int main()
 	for (int i = 0; i < list2.size(); i++)
 		cout << list2[i] << '\t';
 	cout << endl;
+
+	vector<int> list3 = { 3, 4, 7, 4, 6, 5, 8 };
+	cout << "Input array ..." << endl;
+	for (int i = 0; i < list3.size(); i++)
+		cout << list3[i] << '\t';
+	cout << endl;
+
+	InsertionSort(list3);
+
+	cout << "Sorted array ..." << endl;
+	for (int i = 0; i < list3.size(); i++)
+		cout << list3[i] << '\t';
+	cout << endl;
+
+	cout << binar_search(list3, 5);
 }
 
